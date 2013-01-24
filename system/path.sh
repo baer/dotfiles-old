d7857da41747fa69ad367a9733fc57849acfaeb3
Eric Baer <me@ericbaer.com>
2013-01-23 17:00:23 -0700

    Converting zsh->bash

---
+++ system/path.sh
@@ -0,0 +1,3 @@
+export PATH="/usr/local/heroku/bin:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$DOTFILES/bin:$PATH"
+
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
