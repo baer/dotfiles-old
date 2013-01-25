e53ee9f1672de780bec58d8927d380b6d305189c
Eric Baer <me@ericbaer.com>
2013-01-25 14:51:30 -0700

    Bash profile and config cleanup

--- bash/config.sh
+++ bash/config.sh
@@ -1,3 +1,7 @@
+# ----------------------------------------------------------------------
+# History
+# ----------------------------------------------------------------------
+
 # don't put duplicate lines in the history and keep it around
 export HISTCONTROL=ignoreboth:erasedups:ignoredups
 export HISTSIZE=100000
@@ -5,6 +9,21 @@ export HISTIGNORE="ll:clear:code"
 
 # append to the history file, rather than overwriting it
 shopt -s histappend
+
+# ----------------------------------------------------------------------
+# Language and Colors
+# ----------------------------------------------------------------------
+
+# enable en_US locale w/ utf-8 encodings if not already configured
+: ${LANG:="en_US.UTF-8"}
+: ${LANGUAGE:="en"}
+: ${LC_CTYPE:="en_US.UTF-8"}
+: ${LC_ALL:="en_US.UTF-8"}
+export LANG LANGUAGE LC_CTYPE LC_ALL
+
+# ----------------------------------------------------------------------
+# Misc
+# ----------------------------------------------------------------------
 
 # Case-insensitive globbing (used in pathname expansion)
 shopt -s nocaseglob
@@ -23,10 +42,6 @@ for option in autocd globstar; do
   shopt -s "$option" 2> /dev/null
 done
 
-# Prefer US English and use UTF-8
-export LC_ALL="en_US.UTF-8"
-export LANG="en_US"
-
 # Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
 [ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2)" scp sftp ssh
 
@@ -37,19 +52,3 @@ complete -W "NSGlobalDomain" defaults
 # Add `killall` tab completion for common apps
 complete -o "nospace" -W "Contacts Calendar Dock Finder Mail Safari iTunes SystemUIServer Terminal Twitter" killall
 
-# make less more friendly for non-text input files, see lesspipe(1)
-#[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
-
-# enable color support of specific commands
-if [ -x /usr/bin/dircolors ]; then
-    eval "`dircolors -b`"
-    alias ls='ls --color=auto'
-    alias dir='dir --color=auto'
-    alias vdir='vdir --color=auto'
-
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
-fi
-
-source ~/.ssh/ssh-agent-setup.sh
--- system/env.sh
+++ system/env.sh
@@ -1,1 +1,9 @@
-export EDITOR='subl'
+# See what we have to work with ...
+HAVE_VIM=$(command -v vim)
+HAVE_GVIM=$(command -v gvim)
+
+# EDITOR
+test -n "$HAVE_VIM" &&
+EDITOR=vim ||
+EDITOR=vi
+export EDITOR
