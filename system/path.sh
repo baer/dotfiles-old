67364f6d322b96bd301986d2b97a19a92caf9889
Eric Baer <me@ericbaer.com>
2013-09-29 20:35:46 -0700

    Remove unused files

--- system/path.sh
+++ system/path.sh
@@ -1,3 +1,3 @@
-export PATH="/usr/local/share/npm/bin:/usr/local/heroku/bin:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$HOME/dotfiles/bin:$HOME/bin:$PATH"
+export PATH="/usr/local/share/npm/bin:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$HOME/dotfiles/bin:$HOME/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
